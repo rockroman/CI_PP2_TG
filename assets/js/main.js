d638b5ade4da71796531567ace8be02ebdfb6163
rockroman <rock.rakic@gmail.com>
2022-08-30 15:51:45 +0000

    Add pop up screen variables for 1st game

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+// in game variables
+
+// pop up modal variables
+let welcome_Screen=document.getElementById('welcome-screen');
+let start_Modal=document.getElementById("start-game-modal");
+let game_Over= document.getElementById('game-over');
+let next_Btn=document.getElementById('next');
+let play_Btn=document.getElementById('play');
+let try_Again=document.getElementById('try-again');
+let play_Quiz=document.getElementById('play-quiz');
