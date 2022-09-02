891db8f7096528eb0f275adecb794ae831200be9
rockroman <rock.rakic@gmail.com>
2022-09-02 17:06:09 +0000

    ACreate quiz game variables

---
+++ assets/js/quiz.js
@@ -0,0 +1,13 @@
+/**
+ * in game variables
+ * Element variables
+ */
+let question=document.getElementById('question');
+let choices=Array.from(document.getElementsByClassName('choice-text'));
+let progressBarFull=document.getElementById('progressBarFull');
+let userScore=document.getElementById('userScore');
+let nextQuestion=document.getElementById('next');
+
+/**
+ * functionality variables
+ */
