ebe49b2c2c099fee40cca5b6d1618834d769dc83
rockroman <rock.rakic@gmail.com>
2022-09-02 23:49:06 +0000

    Add questions to questions js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,452 +1,463 @@
 let allQuestions=[
 
     {
+        question:'What do you call the thickest string or the loudest sounding (6th String)?',
+        choice1:'A',
+        choice2:'B',
+        choice3:'D',
+        choice4:'E',
+        answer:4
+    },
+
+    {
+        question:'Which type of socket can you find on an electric guitar?',
+        choice1:'Bob',
+        choice2:'Jack',
+        choice3:'Richard',
+        choice4:'Ed',
+        answer:2
+    },
+
+    {
+        question:'Which band was Brian May in?',
+        choice1:'King',
+        choice2:'Ac/Dc',
+        choice3:'Queen',
+        choice4:'Beatles',
+        answer:3
+    },
+
+    {
+        question:'Who was not a member of the Yardbirds?',
+        choice1:'Jimmy Page',
+        choice2:'Eric Clapton',
+        choice3:'Jeff Beck',
+        choice4:'David Gilmour',
+        answer:4
+    },
+
+    {
+        question:'What year was the electric guitar introduced?',
+        choice1:'1897',
+        choice2:'1925',
+        choice3:'1929',
+        choice4:'1931',
+        answer:4
+    },
+
+    {
+        question:'What is the highest string count on a playable guitar?',
+        choice1:'24',
+        choice2:'14',
+        choice3:'42',
+        choice4:'12',
+        answer:3
+    },
+
+    {
+        question:' This is not a type of guitar.',
+        choice1:'Bass',
+        choice2:'Acoustic',
+        choice3:'Fife',
+        choice4:'Electric',
+        answer:3
+    },
+
+    {
+        question:'This man was a pioneer and designer of the electric guitar.',
+        choice1:'Les Paul',
+        choice2:'Ty Tabor',
+        choice3:'Chuck Berry',
+        choice4:'Yngwie Malmsteen',
+        answer:1
+    },
+
+    {
+        question:' makes the sound of the guitar fuzzier?',
+        choice1:'Phase Shifter',
+        choice2:'Wah Pedal',
+        choice3:'Equalizer',
+        choice4:'Distortion Pedal',
+        answer:4
+    },
+
+    {
+        question:'What was the first model guitar produced by Fender?',
+        choice1:'Broadcaster',
+        choice2:'Jaguar',
+        choice3:'Telecaster',
+        choice4:'Stratocaster',
+        answer:1
+    },
+
+    {
+        question:' Who plays "Lucille"?',
+        choice1:'B. B. King',
+        choice2:'Albert Collins',
+        choice3:'Bo Diddley',
+        choice4:'Gatemouth Brown',
+        answer:1
+    },
+
+    {
+        question:'Who plays the "Red Special"?',
+        choice1:'Kirk Hammett',
+        choice2:'Brian May',
+        choice3:'The Edge',
+        choice4:'Ted Nugent',
+        answer:2
+    },
+
+    {
+        question:' A guitar is usually considered part of this family of instruments.',
+        choice1:'Lute',
+        choice2:'Brass',
+        choice3:'Woodwinds',
+        choice4:'Percussion',
+        answer:1
+    },
+
+    {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
     {
         question:'',
         choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
+        choice2:'',
+        choice3:'',
+        choice4:'',
         answer:
     },
 
-    {
-        question:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        answer:
-    },
-
-    {
-        question:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        answer:
-    },
-
-    {
-        question:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        answer:
-    },
-
-    {
-        question:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        answer:
-    },
-
-    {
-        question:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        answer:
-    },
-
-    {
-        question:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        answer:
-    },
-
-    {
-        question:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        answer:
-    },
-
-    {
-        question:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        answer:
-    },
-
-    {
-        question:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        answer:
-    },
-
-    {
-        question:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        answer:
-    },
-
-    {
-        question:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        answer:
-    },
-
-    {
-        question:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        choice1:'',
-        answer:
-    }
+    
 ]
